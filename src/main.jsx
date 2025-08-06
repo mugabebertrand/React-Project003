320319c6d3f81bc9005fb7ef1556834b99c0b88a
mugabebertrand <mugabebertrand@gmail.com>
2025-08-05 22:38:07 -0500

    Add ScrollTopTop.jsx file and update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import ScrollToTop from './ScrollToTop';  
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />   
       <App />
     </BrowserRouter>
-  </StrictMode>
+  </React.StrictMode>
 );
 
